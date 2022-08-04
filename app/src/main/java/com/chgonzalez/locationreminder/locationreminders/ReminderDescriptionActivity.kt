648722993b41229f886a5b7ebb4181a7c15bd45f
christian <thech999@gmail.com>
2022-08-04 20:28:08 +0100

    Base project

---
+++ app/src/main/java/com/chgonzalez/locationreminder/locationreminders/ReminderDescriptionActivity.kt
@@ -0,0 +1,36 @@
+package com.chgonzalez.locationreminder.locationreminders
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.chgonzalez.locationreminder.R
+import com.chgonzalez.locationreminder.databinding.ActivityReminderDescriptionBinding
+import com.chgonzalez.locationreminder.locationreminders.reminderslist.ReminderDataItem
+/**
+ * Activity that displays the reminder details after the user clicks on the notification
+ */
+class ReminderDescriptionActivity : AppCompatActivity() {
+
+    companion object {
+        private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
+
+        //        receive the reminder object after the user clicks on the notification
+        fun newIntent(context: Context, reminderDataItem: ReminderDataItem): Intent {
+            val intent = Intent(context, ReminderDescriptionActivity::class.java)
+            intent.putExtra(EXTRA_ReminderDataItem, reminderDataItem)
+            return intent
+        }
+    }
+
+    private lateinit var binding: ActivityReminderDescriptionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_reminder_description
+        )
+//        TODO: Add the implementation of the reminder details
+    }
+}
