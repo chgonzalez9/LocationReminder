59f2a0fe78cf0791559ec34f8c9468fd4ef10143
Christian <thech999@gmail.com>
2022-09-15 00:15:10 +0100

    BACKGROUND_LOCATION_PERMISSION_INDEX fixed

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -345,4 +345,4 @@ private const val TAG = "SaveReminderActivity"
 private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
 private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
 private const val LOCATION_PERMISSION_INDEX = 0
-private const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
+private const val BACKGROUND_LOCATION_PERMISSION_INDEX = -1
